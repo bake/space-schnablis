f89c939fa843ecb246fded29d88ac21fe4cd98ce
bake <bake@192k.pw>
2019-04-18 12:42:46 +0200

    add a firework 🎆

--- schnablikarte.ino
+++ schnablikarte.ino
@@ -1,13 +1,32 @@
-#include "MCUFRIEND_kbv.h"
+#include <TouchScreen.h>
+#include <MCUFRIEND_kbv.h>
 MCUFRIEND_kbv tft;
 
 #include "space_schnabli_100.c"
 
 #define BLACK 0x0000
 #define WHITE 0xffff
+#define RED 0xe000
+#define ORANGE 0xfc60
+#define YELLOW 0xff60
+#define GREEN 0x0404
+#define BLUE 0x027f
+#define VIOLET 0x7030
 
 #define WIDTH 480
 #define HEIGHT 320
+
+#define MINPRESSURE 200
+#define MAXPRESSURE 1000
+
+// Number of frames a spark is alive.
+#define SPARK_AGE 25
+#define SPARKS_PER_TAP 8
+
+// Frames between spawning fireworks.
+#define FIREWORK_CD 5
+// Chance that a random firework explodes.
+#define FIREWORK_CHANCE 25
 
 struct entity_t
 {
@@ -22,6 +41,30 @@ struct star_t
 	float x, y;
 	float dx;
 };
+
+struct spark_t
+{
+	float x, y;
+	float dx, dy;
+	int8_t age;
+	int16_t color;
+};
+
+const int XP = 8, XM = A2, YP = A3, YM = 9;
+const int TS_LEFT = 97, TS_RT = 949, TS_BOT = 128, TS_TOP = 905;
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+entity_t schnablis[3];
+uint8_t schnablis_num = sizeof(schnablis) / sizeof(*schnablis);
+
+star_t stars[25];
+uint8_t stars_num = sizeof(stars) / sizeof(*stars);
+
+spark_t sparks[64];
+uint8_t sparks_num = sizeof(sparks) / sizeof(*sparks);
+
+uint16_t spark_colors[] = {RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET};
+uint8_t spark_colors_num = sizeof(spark_colors) / sizeof(uint16_t);
 
 /* entity_t */
 
@@ -152,13 +195,94 @@ void star_t_draw(star_t *s)
 	tft.drawPixel(s->x, s->y, WHITE);
 }
 
+/* sparks */
+
+void spark_t_update(spark_t *s)
+{
+	spark_t_clear(s);
+	if (s->age <= 0)
+	{
+		return;
+	}
+	spark_t_move(s);
+	spark_t_draw(s);
+}
+
+void spark_t_move(spark_t *s)
+{
+	s->x += s->dx;
+	s->y += s->dy;
+	s->age--;
+	if (WIDTH < s->x || s->x < 0)
+	{
+		s->age = 0;
+	}
+	if (HEIGHT < s->y || s->y < 0)
+	{
+		s->age = 0;
+	}
+}
+
+void spark_t_clear(spark_t *s)
+{
+	tft.drawPixel(s->x, s->y, BLACK);
+}
+
+void spark_t_draw(spark_t *s)
+{
+	tft.drawPixel(s->x, s->y, s->color);
+}
+
+/* firework */
+
+int firework_cd = FIREWORK_CD;
+
+// Spawn same colored sparks. If force is true, a soarks are created regardless
+// of the fireworks cooldown.
+void firework_new(int x, int y, bool force)
+{
+	if (!force)
+	{
+		if (firework_cd > 0)
+		{
+			return;
+		}
+		firework_cd = FIREWORK_CD;
+	}
+
+	uint8_t sparks_added = 0;
+	uint16_t color = spark_colors[random(0, spark_colors_num - 1)];
+	for (int i = 0; i < sparks_num; i++)
+	{
+		if (sparks[i].age > 0)
+		{
+			continue;
+		}
+		if (sparks_added > SPARKS_PER_TAP)
+		{
+			break;
+		}
+		sparks_added++;
+		sparks[i].x = x;
+		sparks[i].y = y;
+		sparks[i].dx = random(-200, 200) / 100.0;
+		sparks[i].dy = random(-200, 200) / 100.0;
+		sparks[i].age = SPARK_AGE;
+		sparks[i].color = color;
+	}
+}
+
+void firework_update()
+{
+	firework_cd--;
+
+	if (random(0, 100) < FIREWORK_CHANCE)
+	{
+		firework_new(random(0, WIDTH), random(0, HEIGHT), true);
+	}
+}
+
 /* main */
-
-entity_t schnablis[3];
-uint8_t schnablis_num = sizeof(schnablis) / sizeof(*schnablis);
-
-star_t stars[25];
-uint8_t stars_num = sizeof(stars) / sizeof(*stars);
 
 void setup()
 {
@@ -182,6 +306,8 @@ void setup()
 
 void loop()
 {
+	touch();
+
 	for (int i = 0; i < stars_num; i++)
 	{
 		star_t_update(&stars[i]);
@@ -189,5 +315,25 @@ void loop()
 	for (int i = 0; i < schnablis_num; i++)
 	{
 		entity_t_update(&schnablis[i]);
+	}
+	for (int i = 0; i < sparks_num; i++)
+	{
+		spark_t_update(&sparks[i]);
+	}
+	firework_update();
+}
+
+void touch()
+{
+	TSPoint p = ts.getPoint();
+	pinMode(YP, OUTPUT);
+	pinMode(XM, OUTPUT);
+	digitalWrite(YP, HIGH);
+	digitalWrite(XM, HIGH);
+	if (MINPRESSURE < p.z && p.z < MAXPRESSURE)
+	{
+		int x = map(p.y, TS_LEFT, TS_RT, 0, WIDTH);
+		int y = map(p.x, TS_BOT, TS_TOP, 0, HEIGHT);
+		firework_new(x, y, false);
 	}
 }
